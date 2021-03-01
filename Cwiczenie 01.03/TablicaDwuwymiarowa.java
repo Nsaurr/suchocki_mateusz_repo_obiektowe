246ec2d9799720522b7f220036c36ce49ecdb750
Nsaurr <matyszka.suchy@gmail.com>
2021-03-01 09:09:05 +0100

    Stworzenie tablicy dwuwymiarowej

--- Cwiczenie 01.03/TablicaDwuwymiarowa.java
+++ Cwiczenie 01.03/TablicaDwuwymiarowa.java
@@ -1,5 +1,8 @@
 class TablicaDwuwymiarowa{
     public static void main(String [] args){
+        int [] tab =  new int [5];
+        tab[0] = 40;
+        int [][] tab2d = new int[5][2];
         
     }
 }
