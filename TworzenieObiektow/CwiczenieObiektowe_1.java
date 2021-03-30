176986d4d8b66027e2c9fd2501cb63e3bdbdf351
Nsaurr <matyszka.suchy@gmail.com>
2021-03-30 09:13:27 +0200

    Zrobienie drugiego zadania

--- TworzenieObiektow/CwiczenieObiektowe_1.java
+++ TworzenieObiektow/CwiczenieObiektowe_1.java
@@ -1,5 +1,8 @@
 class CwiczenieObiektowe_1{
     public static void main(String [] args){
         MojeDane md = new MojeDane("Mateusz", "Suchocki", "2PTN");
+        Logowanie lg = new Logowanie();
+        String login = lg.logowanie();
+        System.out.println(login);
     }
 }
---
+++ TworzenieObiektow/Logowanie.java
@@ -0,0 +1,7 @@
+class Logowanie {
+    private String login = "akronim";
+    private String password = "123ASDlo9";
+    public String logowanie(){
+        return("Login: "+login+" Password: "+password);
+    }
+}
