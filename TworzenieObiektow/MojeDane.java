e3998ccaf6da3d6e2d87b65df60928bf73167248
Nsaurr <matyszka.suchy@gmail.com>
2021-03-30 09:02:04 +0200

    Stworzenie folderu

---
+++ TworzenieObiektow/MojeDane.java
@@ -0,0 +1,10 @@
+import java.util.jar.Attributes.Name;
+
+class MojeDane{
+    public String name = "";
+    public String second_name = "";
+    public String klasa = "";
+    public MojeDane (String name, String second_name, String klasa){
+        System.out.println("");
+    }
+}
