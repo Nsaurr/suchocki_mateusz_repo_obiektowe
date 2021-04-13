15f8bb19cc7ccfd60efcebf606b9eea70cd29f50
Nsaurr <matyszka.suchy@gmail.com>
2021-04-13 09:49:54 +0200

    Dodanie mozliwosci obliczenia pola z kilku trojkatow

--- TrojkatyZadanie/Main.java
+++ TrojkatyZadanie/Main.java
@@ -1,5 +1,7 @@
 class Main{
     public static void main(String [] args){
-        
+       Triangle t1 = new Triangle(5,10);
+       Triangle t2 = new Triangle(5,10,20);
+       Triangle t3 = new Triangle(5, 10, 5, 10);
     }
 }
--- TrojkatyZadanie/Triangle.java
+++ TrojkatyZadanie/Triangle.java
@@ -1,6 +1,14 @@
 class Triangle {
-    
-    public Traingle(){
-
+    //Pole trojkata z wysokosca i podstawa
+    public Triangle(int wysokosc, int podstawa){
+        System.out.println(wysokosc*podstawa/2);
+    }
+    //Pole trojkata z dwoma bokami oraz katem zwartym miedzy nimi
+    public Triangle(int a, int b, int kat){
+        System.out.println((a*b)/2*Math.sin(kat));
+    }
+    //Pole trojkata z wszystkimi bokami oraz z promieniem
+    public Triangle(int a, int b, int c, int promien){
+        System.out.println((a*b*c)/(4*promien));
     }
 }
