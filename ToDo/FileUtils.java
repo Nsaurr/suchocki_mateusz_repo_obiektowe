18f617ee18e8179b19741cf6aed553ed6a597b9f
Nsaurr <matyszka.suchy@gmail.com>
2021-04-27 11:00:10 +0200

    Dodanie zadania

--- ToDo/FileUtils.java
+++ ToDo/FileUtils.java
@@ -1,48 +1,43 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.FileNotFoundException;
 import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 class FileUtils{
-	/*
-	 * Save ToDo to filename alltodo.csv in format:
-	 * title;content;author;add_date
-	 * @param ToDo - out note
-	 * */
-	public void saveToFile(ToDo td){
-		try{
-			File f = new File("alltodo.csv");
-			FileWriter fw = new FileWriter(f,true);
-			String title = td.getTitle();
-			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm:ss");
-			fw.write(td.getTitle()+";"+td.getContent()+";"+td.getAuthor()
-				+";"+dtf.format(td.getAdd_Date())+"\n");
-			fw.close();
-		} catch(IOException ex){
-			System.out.println("UPS! "+ex.toString());
-		}
-	}
-	
-	public ArrayList<ToDo> readFromFile(){
-		ArrayList<ToDo> list = new ArrayList<>();
-		try{
-			File f = new File("alltodo.csv");
-			Scanner sc = new Scanner(f);
-			while(sc.hasNext()){
-				String temp = sc.nextLine();
-				System.out.println(temp);
-				String [] ttemp = temp.split(";");
-				ToDo td = new ToDo(ttemp[0],ttemp[1],ttemp[2]);
-				
-			}
-		} catch(FileNotFoundException ex){
-			System.out.println("UPS! "+ex.toString());
-		}
-		
-		
-		return list;
-	}
+    /*
+    * Save ToDo to filename alltodo.csv like:
+    * title;content;author;date
+    * @param ToDo - out note
+    * */
+    public void saveToFile(ToDo td){
+        try{
+            File f = new File("alltodo.csv");
+            FileWriter fw = new FileWriter(f, true);
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm:ss");
+            fw.write(td.getTitle()+";"+td.getContent()+";"+td.getAuthor()+";"+dtf.format(td.getDate())+"\n");
+            fw.close();
+        } catch(IOException e){
+            System.out.println(e.toString());
+        }
+    }
+
+    public ArrayList<ToDo> readFromFile(){
+        ArrayList<ToDo> list = new ArrayList<>();
+        try{
+            File f = new File("alltodo.csv");
+            Scanner sc = new Scanner(f);
+            while(sc.hasNext()) {
+                String temp = sc.nextLine();
+                String []ttemp = temp.split(";");
+                ToDo td = new ToDo(ttemp[0], ttemp[1], ttemp[2], LocalDateTime.parse(ttemp[3]));
+            }
+        }catch(FileNotFoundException e){
+            System.out.println(e.toString());
+        }
+        return list;
+    }
 }
--- ToDo/ToDo.java
+++ ToDo/ToDo.java
@@ -1,45 +1,55 @@
 import java.time.LocalDateTime;
 
-class ToDo {
-	private String title = "";
-	private String content = "";
-	private String author = "Anonymous";
-	private LocalDateTime add_date = LocalDateTime.now();
-	
-	//1 Constructor author unknown
-	public ToDo(String title, String content){
-		this.title = title;
-		this.content = content;
-	}
-	
-	//2 Constructor 
-	public ToDo(String title, String content, String author){
-		this.title = title;
-		this.content = content;
-		this.author = author;
-	}
-	
-	public String getContent(){
-		return content;
-	}
-	
-	public LocalDateTime getAdd_Date(){
-		return add_date;
-	}
-	
-	public String getAuthor(){
-		return author;
-	}
-	
-	public String getTitle(){
-		return title;
-	}
-	
-	@Override
-	public String toString(){
-		return "Title: "+title+"\n"
-			+"Content: "+content+"\n"
-			+"Author: "+author+"\n"
-			+"Data: "+add_date+"\n";
-	}
+
+class ToDo{
+    private String title = "";
+    private String content = "";
+    private String author = "Anonymous";
+    private LocalDateTime date;
+
+    //constructor author unknown
+    public ToDo(String title, String content){
+        this.title = title;
+        this.content = content;
+        date = LocalDateTime.now();
+    }
+
+    //constructor author known
+    public ToDo(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        date = LocalDateTime.now();
+    }
+
+    public ToDo(String title, String content, String author, LocalDateTime date){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.date = date;
+    }
+
+    @Override
+    public String toString(){
+        return "Title: "+title+"\n"
+            +"Content: "+content+"\n"
+            +"Author: "+author+"\n"
+            +"Data: "+date+"\n";
+    }
+
+    public String getContent(){
+        return content;
+    }
+
+    public LocalDateTime getDate(){
+        return date;
+    }
+
+    public String getTitle(){
+        return title;
+    }    
+
+    public String getAuthor(){
+        return author;
+    }
 }
